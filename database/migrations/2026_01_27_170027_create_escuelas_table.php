2c882aa38cfb4f50af1359e61b315240ae8c724e
DHM040702 <diegoh04m@gmail.com>
2026-01-27 12:20:32 -0500

    Base de datos

---
+++ database/migrations/2026_01_27_170027_create_escuelas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('escuelas', function (Blueprint $table) {
+            $table->id();
+            $table->string('escuela', 100);
+            //clave foranea
+            $table->foreignId('facultad_id')
+                  ->constrained('facultades')
+                  ->onDelete('cascade');
+            
+            $table->string('usercreacion', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('escuelas');
+    }
+};
