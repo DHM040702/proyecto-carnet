cd0c07f7456c8f728c6130c426bd5071b9359232
DHM040702 <diegoh04m@gmail.com>
2026-01-28 18:50:30 -0500

    Corrige el nombre de la columna 'creado en' a 'fecha_creacion' en la tabla 'estudiantes'

--- database/migrations/2026_01_27_170442_create_estudiantes_table.php
+++ database/migrations/2026_01_27_170442_create_estudiantes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
             $table->string('codigo_estudiante', 12)->unique();
-            $table->date('creado en');
+            $table->date('fecha_creacion');
             $table->string('correo')->unique();
             //Claves foraneas
             $table->foreignId('persona_id')
