7a8af856603795da8007dbd66c9f71b85ed9b5e7
DHM040702 <diegoh04m@gmail.com>
2026-01-31 11:29:27 -0500

    Corrección de Seeders

--- database/seeders/FotosSeeder.php
+++ database/seeders/FotosSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Foto;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Seeder;
 
 class FotosSeeder extends Seeder
@@ -10,8 +11,23 @@ class FotosSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        //Ruta de origen de la foto
+        $origen_4 = database_path('seeders\files\perfil\75840049.jpg');
+
+        //Ruta de destino en las carpetas
+        $rutaDestino_4 = 'fotos\perfil\1_75840049.jpg';
+
+        //Copia de archivo en las carpetas
+        Storage::disk('public')->put(
+            $rutaDestino_4,
+            file_get_contents($origen_4)
+        );
+
+        //Inserción del seeder
+        Foto::insert([
+            ['foto' => 1 , 'ubicacion' => $rutaDestino_4 , 'usercreacion' => 'SEEDER' , 'created_at' => now() , 'updated_at' => now()]
+        ]);
     }
 }
